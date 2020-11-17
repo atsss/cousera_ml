ca20c3b8155f516d91bead67a7d041e29e21aff7
atsss <atsushi0623itoh@gmail.com>
2020-11-16 22:20:25 -0600

    Done 1.2.1 of week03

--- week03/ex2/sigmoid.m
+++ week03/ex2/sigmoid.m
@@ -2,16 +2,26 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid function
 %   g = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+% exp (X) => Compute 'e^x' for each element of X.
+% exp(eye(2))
+% ans =
+%    2.7183   1.0000
+%    1.0000   2.7183
 
+% ./ X => divided by each element of X
+% 1 ./ (1 + exp(eye(2)))
+% ans =
+%    0.26894   0.50000
+%    0.50000   0.26894
 
-
+g = 1 ./ (1 + exp(-z))
 
 % =============================================================
 
