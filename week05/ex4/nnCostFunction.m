e5f4450bb7c78045c5f283df59a391bdca6678f2
atsss <atsushi0623itoh@gmail.com>
2020-11-30 03:41:13 -0600

    Done 2 of week05

--- week05/ex4/nnCostFunction.m
+++ week05/ex4/nnCostFunction.m
@@ -85,6 +85,20 @@ end
 J = -1 / m * sum(sum(Y .* log(hx) + (1 - Y) .* log(1 - hx)));
 J = J + lambda / (2 * m) * (sum(sum(Theta1(:, 2:end) .^ 2)) + sum(sum(Theta2(:, 2:end) .^ 2)));
 
+delta_3 = a_3 - Y;
+% 列数を調整するために ones を追加
+delta_2 = (delta_3 * Theta2) .* sigmoidGradient([ones(size(z_2, 1), 1) z_2]);
+% delta0 は必要ないので削除
+delta_2 = delta_2(:, 2:end);
+
+% 各行の1列目は必要ないのでゼロに置き換える
+reg_2 = lambda / m * [zeros(size(Theta2, 1), 1) Theta2(:, 2:end)];
+reg_1 = lambda / m * [zeros(size(Theta1, 1), 1) Theta1(:, 2:end)];
+
+% 行列で計算することでループも同時に処理する
+Theta2_grad = 1 / m * (delta_3' * a_2) + reg_2;
+Theta1_grad = 1 / m * (delta_2' * a_1) + reg_1;
+
 % -------------------------------------------------------------
 
 % =========================================================================
