c181e659a544e41ec334a2c4eecbd6e7387ab04a
atsss <atsushi0623itoh@gmail.com>
2020-12-10 13:43:31 -0600

    Done 2 of week08

--- week08/ex7/pca.m
+++ week08/ex7/pca.m
@@ -14,17 +14,17 @@ S = zeros(n);
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should first compute the covariance matrix. Then, you
 %               should use the "svd" function to compute the eigenvectors
-%               and eigenvalues of the covariance matrix. 
+%               and eigenvalues of the covariance matrix.
 %
 % Note: When computing the covariance matrix, remember to divide by m (the
 %       number of examples).
 %
 
+% 共分散行列を算出
+sigma = (X' * X) / m;
 
-
-
-
-
+% 共分散行列の固定値を計算
+[U, S] = svd(sigma);
 
 % =========================================================================
 
